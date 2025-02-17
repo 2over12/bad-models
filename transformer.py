518297d5a10d22dbbf43905501cd488952be2fbd
2over12 <2over124@gmail.com>
2025-02-16 21:12:11 -0500

    model no position embeddings

--- transformer.py
+++ transformer.py
@@ -53,9 +53,61 @@ class MultiHeadedAttention(nn.Module):
         # TODO(Ian): make this easily scalable by batching.
         return torch.cat([m(X) for m in self.mods], dims-1)
 
+class FeedForwardNetwork(nn.Module):
+    def __init__(self, input: int, hidden_layer_factor: int, output: int, bias: bool, dropout: float):
+        super().__init__()
+        self.hidden = nn.Linear(input, hidden_layer_factor* input, bias)
+        self.gelu = nn.GELU()
+        self.projections = nn.Linear(hidden_layer_factor * input, output, bias)
+        self.drop = nn.Dropout(dropout)
+    def forward(self, X):
+        mid = self.hidden(X)
+        proj = self.projections(self.gelu(mid))
+        return self.drop(proj)
+
+class Decoder(nn.Module):
+    def __init__(self, num_heads, embedding_size, hidden_layer_factor: int,
+                  bias: bool, dropout: float):
+        super().__init__()
+        self.embedding_size = embedding_size
+        self.num_head = num_heads
+        self.output_size = self.num_head * self.embedding_size
+        self.heads = MultiHeadedAttention(num_heads, embedding_size)
+        self.pointwise_ff = FeedForwardNetwork(self.output_size, 
+                                            hidden_layer_factor, 
+                                            self.embedding_size,
+                                            bias, dropout)
+        self.lnorm1 = nn.LayerNorm(self.embedding_size)
+        self.lnorm2 = nn.LayerNorm(self.output_size)
+
+    def forward(self, X):
+        attn = self.heads(self.lnorm1(X))
+        activation = self.pointwise_ff(self.lnorm2(attn))
+        return activation
+
+class Model(nn.Module):
+    def __init__(self,  vocab_size, _max_seq_len: int, num_decoders, num_heads, 
+                 embedding_size, hidden_layer_factor: int,
+                  bias: bool, dropout: float):
+        super().__init__()
+        self.embedding = nn.Embedding(vocab_size, embedding_size)
+        # TODO(Ian): do this
+        #self.abs_position_embedding = nn.Embedding()
+        self.decs = [Decoder(num_heads, embedding_size, hidden_layer_factor,
+                             bias, dropout) for _ in range(num_decoders)]
+        self.norm = nn.LayerNorm(embedding_size)
+        self.classifier = nn.Linear(embedding_size, vocab_size, bias)
+
+    def forward(self, X_idxs):
+        embs = self.embedding(X_idxs)
+        for dec in self.decs:
+            embs = dec(embs)
+        return self.classifier(self.norm(embs))
+
+
 def main():
-    athd = MultiHeadedAttention(2, 256)
-    mat = torch.rand((5, 256), requires_grad=True)
+    athd = Model(500, 500, 2, 2, 256, 4, False, .1)
+    mat = torch.randint(0, 100, (5,10))
     print(mat)
     print(mat.shape)
     X = athd.forward(mat)
